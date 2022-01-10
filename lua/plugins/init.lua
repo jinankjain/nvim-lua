7ad73da5b977ce2e4ffd4cb4f1594de28955981c
Jinank Jain <jinank94@gmail.com>
2022-01-10 07:49:55 +0000

    Add missing plugins configuration

---
+++ lua/plugins/init.lua
@@ -0,0 +1,92 @@
+local fn = vim.fn
+
+-- Auto install packer on new machine
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use {
+        'wbthomason/packer.nvim'
+    }
+
+    -- Colorscheme
+    use {
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = "vim.cmd('colorscheme rose-pine')"
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ":TSUpdate",
+        event = "BufWinEnter",
+        config = "require('treesitter-config')"
+    }
+
+    use {
+        'hoob3rt/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true},
+        event = "BufWinEnter",
+        config = "require('lualine-config')"
+    }
+
+    use {
+        'akinsho/bufferline.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        event = "BufWinEnter",
+        config = "require('bufferline-config')"
+    }
+
+    -- This provides awesome rainbows {{{{}}}}
+    use {
+        'p00f/nvim-ts-rainbow',
+        after = "nvim-treesitter"
+    }
+
+    use {
+        'b0o/mapx.nvim',
+        before = "which-key.nvim",
+        config = "require('mapx-config')"
+    }
+
+    use {
+        'folke/which-key.nvim',
+        config = "require('which-key-config')"
+    }
+
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons',
+        cmd = "NvimTreeToggle",
+        config = "require('nvim-tree-config')"
+    }
+
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/plenary.nvim'}},
+        cmd = "Telescope",
+        config = "require('telescope-config')"
+    }
+
+    use {'neovim/nvim-lspconfig', config = "require('lsp')"}
+    use {'hrsh7th/cmp-nvim-lsp'}
+    use {'hrsh7th/cmp-buffer'}
+    use {'hrsh7th/nvim-cmp'}
+    use {'hrsh7th/cmp-vsnip'}
+    use {'hrsh7th/vim-vsnip'}
+    use {'onsails/lspkind-nvim'}
+    use {'williamboman/nvim-lsp-installer'}
+    use {'jose-elias-alvarez/null-ls.nvim', config = "require('null-ls-config')"}
+    use {'tami5/lspsaga.nvim', config = "require('lspsaga-config')"}
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
