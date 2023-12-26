6316e7d14b2b2ff00c0dde5ce1090d39b74f831a
Jinank Jain <jinankjain@microsoft.com>
2023-12-26 11:30:21 +0000

    Add a new plugin called dressing.nvim
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -107,6 +107,8 @@ return require('packer').startup(function(use)
         end
     }
 
+    use { 'stevearc/dressing.nvim' }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
