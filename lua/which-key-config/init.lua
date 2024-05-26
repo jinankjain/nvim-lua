3fc2bd4fbe4b729b76b971c0f8b4dd59e94a9744
Jinank Jain <jinank94@gmail.com>
2024-05-26 10:03:36 +0530

    Change the live grep mapping
    Signed-off-by: Jinank Jain <jinank94@gmail.com>

--- lua/which-key-config/init.lua
+++ lua/which-key-config/init.lua
@@ -68,7 +68,7 @@ vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 -- telescope keymappings
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fc', '<cmd>Telescope grep_string<cr>', { desc = "Find string under cursor in cwd" })
